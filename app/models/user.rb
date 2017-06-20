9f978a1581482e0896df2f73fea32f42f2ec3ebf
Agustin Pagnoni <aguspagnoni@hotmail.com>
2017-06-20 15:55:37 -0300

    Added Status enum to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   belongs_to :company
   mount_uploader :image, PictureUploader
   validates :email, presence: true, email: true
+  enum status: [:not_confirmed, :confirmed]
 
   has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619044445) do
+ActiveRecord::Schema.define(version: 20170620185450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -145,10 +145,11 @@ ActiveRecord::Schema.define(version: 20170619044445) do
     t.string   "cellphone"
     t.string   "job_position"
     t.integer  "company_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.string   "image"
     t.string   "password_digest"
+    t.integer  "status",          default: 0
     t.index ["company_id"], name: "index_users_on_company_id", using: :btree
   end
 
