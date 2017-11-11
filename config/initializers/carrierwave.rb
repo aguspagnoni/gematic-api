074453e9f8ac53fdf00057b328b5045a9dcdb5ea
Agustin Pagnoni <aguspagnoni@hotmail.com>
2017-11-11 11:42:39 -0300

    carrierwave configuration

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,13 +2,10 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'xxx',                        # required
-    aws_secret_access_key: 'yyy',                        # required
-    region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
-    host:                  's3.example.com',             # optional, defaults to nil
-    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    aws_access_key_id:     Rails.application.secrets.s3_access_key,                        # required
+    aws_secret_access_key: Rails.application.secrets.s3_secret_key,                        # required
+    region:                'us-west-2',                  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'name_of_directory'                          # required
-  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_directory  = 'gematic-storage'                          # required
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
