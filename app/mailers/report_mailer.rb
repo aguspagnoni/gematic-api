0077c482fb11c002a79be0084244b9023c60cbdf
Agustin Pagnoni <aguspagnoni@hotmail.com>
2017-12-19 02:01:09 -0300

    Added ReporMailer

---
+++ app/mailers/report_mailer.rb
@@ -0,0 +1,21 @@
+class ReportMailer < ApplicationMailer
+
+  def pricelist_summary(pricelist, recipient)
+    @pricelist = pricelist
+    @recipient = recipient
+    @header_data = header_data_hash
+    mail(to: recipient.email, subject: I18n.t('mailers.pricelist_summary.subject'))
+  end
+
+  private
+
+  def header_data_hash
+    [
+      { label: 'Empresa',                    text: @pricelist.company.name },
+      { label: 'Valido desde (dia/mes/año)', text: @pricelist.valid_since.strftime("%d/%m/%Y") },
+      { label: 'Valido hasta (dia/mes/año)', text: @pricelist.expires.strftime("%d/%m/%Y") },
+      { label: 'Descuento General',          text: @pricelist.general_discount },
+      { label: 'Agente responsable',         text: "#{@recipient.name} (#{@recipient.email})" }
+    ]
+  end
+end
