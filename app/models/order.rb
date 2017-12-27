2aaecc14755e3f1df18e7fc833af46ad8708f2ee
Agustin Pagnoni <aguspagnoni@hotmail.com>
2017-12-27 00:39:10 -0300

    Added OrderItem destroy when destroying an order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  has_many    :order_items
+  has_many    :order_items, dependent: :destroy
   has_many    :products, through: :order_items
   belongs_to  :custom_price_list, class_name: 'PriceList',
                                   foreign_key: 'custom_price_list_id',
