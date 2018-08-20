4ba8e6f4249cb7bd3f7a23278ba918d4d29987a3
Agustin Pagnoni <aguspagnoni@hotmail.com>
2018-08-19 21:40:24 -0300

    made more visible the budget condition of an order

--- spec/mailers/report_mailer_spec.rb
+++ spec/mailers/report_mailer_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ReportMailer, type: :mailer do
+  let(:body)              { mail.attachments.first.body }
+  let(:budget_image_name) { 'borrador.png' }
 
   describe 'pricelist_summary' do
     let(:pricelist)  { create(:price_list_with_company_and_products) }
@@ -8,7 +10,41 @@ RSpec.describe ReportMailer, type: :mailer do
     let(:mail) { ReportMailer.pricelist_summary(pricelist, admin_user) }
 
     it 'renders the html ok' do
-      expect(mail.attachments.first.body).to include(pricelist.discounts.first.product.name)
+      expect(body).to include(pricelist.discounts.first.product.name)
     end
   end
+
+  describe 'order_summary' do
+    let(:pricelist)  { create(:price_list_with_company_and_products) }
+    let(:admin_user) { create(:admin_user) }
+    let(:order)      { create(:order, status: :confirmed) }
+    let(:mail) { ReportMailer.order_summary(order, admin_user) }
+
+    context 'when the order is <<not_confirmed>>' do
+      let(:order) { create(:order, status: :not_confirmed) }
+      it 'renders the html with a special text' do
+        write_file_and_open(body)
+        expect(body).to include(budget_image_name)
+      end
+    end
+
+    context 'when the order is <<with_invoice>>' do
+      let(:order) { create(:order, status: :with_invoice) }
+      it 'renders the html' do
+        expect(body).not_to include(budget_image_name)
+      end
+    end
+
+    it 'renders the html' do
+      expect(body).not_to include(budget_image_name)
+    end
+  end
+end
+
+def write_file_and_open(content)
+  filename = 'tmp/mail_content_spec.html'
+  f = File.new(filename, 'w')
+  f.write(content)
+  f.close
+  `open #{filename}`
 end
